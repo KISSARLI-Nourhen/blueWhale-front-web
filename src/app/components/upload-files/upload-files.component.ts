2abd23ab6bd35b790dcaf10931b915a83a8809f4
KISSARLI-Nourhen <kissarlinourhen2@gmail.com>
2021-10-08 15:27:51 +0200

    clean code

--- src/app/components/upload-files/upload-files.component.ts
+++ src/app/components/upload-files/upload-files.component.ts
@@ -61,7 +61,7 @@ export class UploadFilesComponent implements OnInit {
       },
       err => {
         this.progress = 0;
-        this.message = 'Could not upload the file (size)!';
+        this.message = 'Could not upload the file!';
         this.currentFile = undefined;
       });
 
